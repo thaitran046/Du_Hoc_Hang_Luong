ca3bbaf03819edad697c0e7a06d7835cde179614
tran quoc thai <thaitran@MacBook-Air-cua-tran.local>
2026-09-03 10:20:27 +0700

    Them Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import './styles/global.css'
 import HomePage from './routes/index.jsx'
 import ThankYouPage from './routes/cam-on.jsx'
 import { initTracking } from './lib/tracking.js'
+import { inject } from '@vercel/analytics' // 1. Thêm dòng này
+
+inject() // 2. Thêm dòng này để kích hoạt tracking
+
 
 initTracking()
 
